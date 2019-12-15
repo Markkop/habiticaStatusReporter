05c79149ae8c9caffbd3801533b7dd0dee12d2fb
Markkop <mk.marcelo.mk@gmail.com>
2019-12-15 12:20:16 -0300

    add babel configuration

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('Hello World!')
+
+const nestedArray = ['ES10', ['is', 'working!']]
+const flattedArray = nestedArray.flat() // ES10 Array.prototype.flat() function
+console.log(flattedArray.join(' '))
+
+// Now, if you try to 'nodemon index.js', you'll get 'TypeError: nestedArray.flat is not a function'
+// But if 'nodemon --exec babel-node index.js', the code will be transpiled with EcmaScript2019
