37667c78e501510c07fa6d10a50cd29d23311470
Markkop <mk.marcelo.mk@gmail.com>
2019-12-15 15:16:19 -0300

    QUEST #3: send message to twitter

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ Here we'll be building a bot that checks a character's attributes on Habitica an
 2. Create a copy of `.envexample`, rename it to `.env` and add your credentials inside.
 3. Run `npm run dev`
 
+## How to aquire twitter tokens
+
+1. [Apply for twitter developer](https://developer.twitter.com/en/apply)
+2. [Generate tokens](https://developer.twitter.com/en/docs/basics/authentication/guides/access-tokens)
+3. We'll be using [OAuth](https://www.npmjs.com/package/oauth) for [authentication](https://developer.twitter.com/en/docs/basics/authentication/overview/oauth)
+
 ## How to aquire habitica userid
 
 1. Copy User ID from this [settings page](https://habitica.com/user/settings/api)
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import axios from 'axios'
+import OAuth from 'oauth'
 
 const getStats = async (userid) => {
     try {
@@ -24,11 +25,40 @@ const selectMessage = ({ hp = 0, maxHealth = 0, exp = 0, toNextLevel = 0 }) => {
     return `Things are fine for now. ${status}`
 }
 
+const reportTwitter = async (message) => {
+    const oauth = new OAuth.OAuth(
+        'https://api.twitter.com/oauth/request_token',
+        'https://api.twitter.com/oauth/access_token',
+        process.env.TWITTER_CONSUMER_APIKEY,
+        process.env.TWITTER_CONSUMER_APISECRETKEY,
+        '1.0A',
+        null,
+        'HMAC-SHA1'
+    );
+
+    return oauth.post(
+        'https://api.twitter.com/1.1/statuses/update.json',
+        process.env.TWITTER_ACCESS_TOKEN,
+        process.env.TWITTER_ACCESS_SECRETTOKEN,
+        { status: message },
+        'application/x-www-form-urlencoded',
+        function callback(error, data, res) {
+            if (error) {
+                throw new Error(error.data)
+            };
+
+            const jsonData = JSON.parse(data)
+            const { user: { screen_name }, text } = jsonData
+            console.log(`Tweet created! @${screen_name}: ${text}`)
+            return true
+        });
+}
+
 const reportStatus = async () => {
     try {
         const stats = await getStats(process.env.HABITICA_USERID)
         const message = selectMessage(stats)
-        console.log(message)
+        return reportTwitter(message)
     } catch (error) {
         console.log(error)
     }
